bd7cf8a482bb134f004e45f4ec729b957300abb9
Alice Davis <alice@gigantic.computer>
2019-02-13 18:41:32 -0800

    chore: scaffold DropdownWithPills component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <DropdownWithPills :options="options" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import options from "./options";
+import DropdownWithPills from "./components/DropdownWithPills.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
-  }
+    DropdownWithPills
+  },
+  data: () => ({ options })
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
